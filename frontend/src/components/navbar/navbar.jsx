9287c7a23d68dbaf88249c1584313d84c3b76b72
TunaSaygin <tuna.saygin@ug.bilkent.edu.tr>
2024-04-21 22:52:45 +0300

    ups

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
 import { useAuth } from '../../context/authcontext';
-import { Dropdown } from 'react-bootstrap';
+import { Dropdown, Nav } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+
 export default function Navbar() {
   const { isLoggedIn, logout } = useAuth(); // Use the useAuth hook
   const nav = useNavigate();
-  const hanndleLogout = ()=>{
-    logout()
-    nav("login")
-  }
+  const handleLogout = () => {
+    logout();
+    nav("/login");
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light p-3">
       <div className="container-fluid">
@@ -18,37 +20,44 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul className="navbar-nav">
-          {isLoggedIn ? (
-            <>
-              {/* Show logout and notifications if logged in */}
-              <li className="nav-item">
-                <Dropdown>
-                  <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Notifications
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="#!">Notification 1</Dropdown.Item>
-                    {/* Add more notifications here */}
-                  </Dropdown.Menu>
-                </Dropdown>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#" onClick={hanndleLogout}>Logout</a>
-              </li>
-            </>
-          ) : (
-            <>
-              {/* Show login or other links if not logged in */}
-              <li className="nav-item">
-                <a className="nav-link" href="#">Login</a>
-              </li>
-              {/* Add any other public links here */}
-            </>
-          )}
-        </ul>
+          <ul className="navbar-nav">
+            {isLoggedIn ? (
+              <>
+                <li className="nav-item">
+                  <Dropdown>
+                    <Dropdown.Toggle variant="success" id="notifications-dropdown">
+                      Notifications
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="#!">Notification 1</Dropdown.Item>
+                      {/* Add more notifications here */}
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </li>
+                <li className="nav-item">
+                  <Dropdown>
+                    <Dropdown.Toggle variant="success" id="profile-dropdown">
+                      Profile
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="#profile">My Profile</Dropdown.Item>
+                      <Dropdown.Item href="#purchases">Past Purchases</Dropdown.Item>
+                      <Dropdown.Item href="#!" onClick={handleLogout}>Logout</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Nav.Link href="/login">Login</Nav.Link>
+                </li>
+                {/* Add any other public links here */}
+              </>
+            )}
+          </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
